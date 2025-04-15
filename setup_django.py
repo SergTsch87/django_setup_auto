8df2cba06ea625d9691c3dc88bd4975c7c24af4f
SergTsch87 <sergtsch87@gmail.com>
2025-04-15 19:56:29 +0300

    Add: Add new func run_command

--- setup_django.py
+++ setup_django.py
@@ -5,12 +5,31 @@ import platform
 from  pathlib import Path
 
 
-def get_os():
-    return platform.system()
+# # ------------------------------------
+# # Old funcs:
+# def get_os():
+#     return platform.system()
 
 
-def is_file(path: Path) -> bool:
-    return path.is_file()
+# def is_file(path: Path) -> bool:
+#     return path.is_file()
+# # --------------------------------------
+
+PROJECT_NAME = "my_prj"     # Change it!
+CREATE_APP = "tutors_app"   # Change it!
+VENV_NAME = ".venv"
+TARGET_FOLDER = "."         # Current dir
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+def run_command(command, cwd=None):
+    try:
+        print(f'-> {command}')
+        subprocess.run(command, shell=True, cwd=cwd, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"\n❌ Помилка при виконанні команди: {command}")
+        print(f"   Код завершення: {e.returncode}")
+        sys.exit(1)
 
 
 def main():
