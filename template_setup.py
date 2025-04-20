fc1403370c37da18291d3453e71d521dcd4489ae
SergTsch87 <sergtsch87@gmail.com>
2025-04-20 23:04:08 +0300

    Upd: Moved some funcs to files dj_conf.py and template_setup.py

---
+++ template_setup.py
@@ -0,0 +1,25 @@
+# ================= TEMPLATE =====================
+import os
+import constants as cnst
+
+
+def ensure_template_dir_created():
+    template_path = os.path.join(cnst.BASE_DIR, cnst.PROJECT_NAME, cnst.CREATE_APP, 'templates', cnst.CREATE_APP)
+    if os.path.exists(template_path):
+        print(f"⚠️  Django-шаблон templates/'{cnst.CREATE_APP}' вже існує. Пропускаємо створення.")
+        return
+    # manage_py = os.path.join(cnst.BASE_DIR, "manage.py")
+    
+    # utls.run_command(f'mkdir {template_path}')
+    os.makedirs(template_path, exist_ok=True)
+
+
+def ensure_template_file_created(path_file, content):
+    with open(path_file, 'w') as f:
+        f.write(content)
+    # with open(cnst.BASE_TEMPLATE_FILE, 'w'):   # Створює порожній файл
+    #     pass
+    
+    # extend_template = '{% extends "base.html" %}'
+    # with open(cnst.EXTEND_TEMPLATE_FILE, 'w') as f:
+    #     f.write(extend_template)
