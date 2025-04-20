e20ee6d28d59f14b73e0a0e81d6af8b9ae285d2a
SergTsch87 <sergtsch87@gmail.com>
2025-04-20 23:17:31 +0300

    Fix: Fixed all file links

--- setup_django.py
+++ setup_django.py
@@ -1,48 +1,50 @@
 import os
-import sys
-import subprocess
+# import sys
+# import subprocess
 import constants as cnst
-import utils as utls
+# import utils as utls
+import dj_conf as dj
+import template_setup as tmpl
+import views_setup as vw
+import urls_setup as urlstp
 
-# utils:
-# utls.run_command
-# utls.write_content_to_file
-# utls.write_to_gitignore
+# urlstp.create_urls_py_app_cbv
+# urlstp.append_to_urls_py_cbv
 
-# constants:
-# cnst.PROJECT_NAME
-# cnst.CREATE_APP
-# cnst.VENV_NAME
-# cnst.TARGET_FOLDER
-# cnst.DIR_DJANGO
-# cnst.BASE_DIR
-# cnst.BASE_TEMPLATE_FILE
-# cnst.EXTEND_TEMPLATE_FILE
-# cnst.content_for_base_templ_file
-# cnst.content_for_extend_templ_file
+# vw.append_to_cbv_py
 
+# tmpl.ensure_template_dir_created
+# tmpl.ensure_template_file_created
+
+# dj.ensure_django_admin_installed
+# dj.ensure_project_created
+# dj.install_django_and_bootstrap_in_venv
+# dj.ensure_venv_created
+# dj.ensure_app_created
+# dj.get_num_line_with_text
+# dj.write_text_by_num_line
 
 # ================= MAIN =====================
 def main():
     os.makedirs(cnst.DIR_DJANGO, exist_ok=True)
-    ensure_django_admin_installed()   # Встановлює pipx та django
-    ensure_project_created()          # Створює Django-проєкт. Інакше - минає цей етап
-    pip_path, python_path = ensure_venv_created()  # Створює venv
-    install_django_and_bootstrap_in_venv(pip_path)
-    ensure_app_created(python_path)   # Створює Django-застосунок. Інакше - минає цей етап
-    write_text_by_num_line()
+    dj.ensure_django_admin_installed()   # Встановлює pipx та django
+    dj.ensure_project_created()          # Створює Django-проєкт. Інакше - минає цей етап
+    pip_path, python_path = dj.ensure_venv_created()  # Створює venv
+    dj.install_django_and_bootstrap_in_venv(pip_path)
+    dj.ensure_app_created(python_path)   # Створює Django-застосунок. Інакше - минає цей етап
+    dj.write_text_by_num_line()
     print("\n✅ Готово! Django-проєкт і застосунок створено успішно.")
 
     # + додай можливість одразу створити шаблони, view, route або підключити Bootstrap
-    ensure_template_dir_created()         # Створює Django-шаблон. Інакше - минає цей етап
+    tmpl.ensure_template_dir_created()         # Створює Django-шаблон. Інакше - минає цей етап
     
     # Створює HTML-файли Django-шаблону зі шляхом path_file та вмістом content
-    ensure_template_file_created(cnst.BASE_TEMPLATE_FILE, cnst.content_for_base_templ_file)
-    ensure_template_file_created(cnst.EXTEND_TEMPLATE_FILE, cnst.content_for_extend_templ_file)
+    tmpl.ensure_template_file_created(cnst.BASE_TEMPLATE_FILE, cnst.content_for_base_templ_file)
+    tmpl.ensure_template_file_created(cnst.EXTEND_TEMPLATE_FILE, cnst.content_for_extend_templ_file)
     
-    append_to_cbv_py()                    # Дописує CBV-функцію до view
-    append_to_urls_py_cbv()               # Дописує route до списку urlpatterns файлу prj/urls.py
-    create_urls_py_app_cbv()              # Створює app/urls.py з відповідним вмістом
+    vw.append_to_cbv_py()                    # Дописує CBV-функцію до view
+    urlstp.append_to_urls_py_cbv()               # Дописує route до списку urlpatterns файлу prj/urls.py
+    urlstp.create_urls_py_app_cbv()              # Створює app/urls.py з відповідним вмістом
 
     # utls.write_to_gitignore()                  # Розкоментуй, коли тре буде пушити Django-проєкт
 
