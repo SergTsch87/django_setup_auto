a51ce4778d30f4c7f48fbea90282248b2a6e6c4a
SergTsch87 <sergtsch87@gmail.com>
2025-04-20 23:06:48 +0300

    Upd: Moved some funcs to files urls_setup.py and views_setup.py

---
+++ views_setup.py
@@ -0,0 +1,21 @@
+# ================= VIEWS =====================
+import constants as cnst
+import os
+
+
+def append_to_cbv_py():
+    views_path = os.path.join(cnst.BASE_DIR, cnst.CREATE_APP, 'views.py')
+    content = f"""
+    # with templates
+    # Class-Based View (CBV):
+    from django.views.generic import TemplateView
+
+
+    class HomeView(TemplateView):
+        template_name = '{cnst.EXTEND_TEMPLATE_FILE}'
+    """
+
+    with open(views_path, 'a') as f:
+        f.write(content)
+    print(f"✅ CBV додано до {views_path}")
+    # utls.run_command(f'echo {content_view} >> {cnst.CREATE_APP}/views.py')
